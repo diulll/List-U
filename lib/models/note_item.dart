c3ebd7bcba28e94552f96572d27d9cee55206629
diulll <badiulfikri24@gmail.com>
2026-02-07 12:18:45 +0700

    feat: Add Hive annotations to NoteItem model for persistent storage

--- lib/models/note_item.dart
+++ lib/models/note_item.dart
@@ -1,11 +1,17 @@
-class NoteItem {
+import 'package:hive/hive.dart';
+
+part 'note_item.g.dart';
+
+@HiveType(typeId: 0)
+class NoteItem extends HiveObject {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   String content;
+
+  @HiveField(2)
   String date;
 
-  NoteItem({
-    required this.title,
-    required this.content,
-    required this.date,
-  });
+  NoteItem({required this.title, required this.content, required this.date});
 }
