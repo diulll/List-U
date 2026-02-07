8b588faf4edcc3b697de3b7a75863b50e83fb94f
diulll <badiulfikri24@gmail.com>
2026-02-07 12:19:30 +0700

    feat: Add Hive annotations and isCompleted field to DueItem model

--- lib/models/due_item.dart
+++ lib/models/due_item.dart
@@ -1,11 +1,25 @@
-class DueItem {
+import 'package:hive/hive.dart';
+
+part 'due_item.g.dart';
+
+@HiveType(typeId: 1)
+class DueItem extends HiveObject {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   String dueDate;
+
+  @HiveField(2)
   String priority;
+
+  @HiveField(3)
+  bool isCompleted;
 
   DueItem({
     required this.title,
     required this.dueDate,
     this.priority = 'Medium',
+    this.isCompleted = false,
   });
 }
