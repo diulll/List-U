aa1ad300b4e0691f59488a967f0bec438f4ad941
diulll <badiulfikri24@gmail.com>
2026-02-07 12:20:53 +0700

    feat: Add Hive annotations to TodoItem model for persistent storage

--- lib/models/todo_item.dart
+++ lib/models/todo_item.dart
@@ -1,11 +1,17 @@
-class TodoItem {
+import 'package:hive/hive.dart';
+
+part 'todo_item.g.dart';
+
+@HiveType(typeId: 2)
+class TodoItem extends HiveObject {
+  @HiveField(0)
   String title;
+
+  @HiveField(1)
   String time;
+
+  @HiveField(2)
   bool isCompleted;
 
-  TodoItem({
-    required this.title,
-    required this.time,
-    this.isCompleted = false,
-  });
+  TodoItem({required this.title, required this.time, this.isCompleted = false});
 }
